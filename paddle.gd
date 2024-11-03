4263a8941e187cb711174e5900e38f06463a89e6
Koyama <proteinkoyama@gmail.com>
2024-11-04 08:58:53 +0900

    update

--- paddle.gd
+++ paddle.gd
@@ -9,7 +9,7 @@ var character_size= Vector2.ZERO
 func _ready():
 	screen_size = get_viewport_rect().size
 	character_size = $ColorRect.size  # スプライトの半分の大きさ
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
@@ -19,10 +19,3 @@ func _process(delta: float) -> void:
 
 	position += velocity.normalized() * speed * delta
 	position.x = clamp(position.x, 0, screen_size.x - character_size.x )
-
-
-
-func _on_body_dentered(_body):
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
