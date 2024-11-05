b18b777b6f77eea7bbca2aba6566f4087966b8d3
Koyama <proteinkoyama@gmail.com>
2024-11-06 08:51:02 +0900

    update

--- ball.gd
+++ ball.gd
@@ -10,19 +10,11 @@ func _process(_float) -> void:
 
 func _on_body_entered(body):
 	print(linear_velocity)
-	# 法線に基づく反射ベクトルを計算
-	var collision_normal = (position - body.position).normalized()
-	var bounce_velocity = linear_velocity.bounce(collision_normal)
-		# 最小のy方向速度を設定して水平な跳ね返りを防ぐ
-	if abs(bounce_velocity.y) < 50:
-		bounce_velocity.y = -sign(bounce_velocity.y) * 50  # Y成分の最低速度
-		
-	linear_velocity = bounce_velocity
+	if linear_velocity <= Vector2(abs(1),abs(1)):
+		linear_velocity.y = 300
 	if body.is_in_group("Block"):
 		body.queue_free() 
 		linear_velocity = linear_velocity.normalized() * (initial_speed + randf_range(0,50))
-
-
 func _integrate_forces(_state):
 	# 常に一定の速度を維持する
 	linear_velocity = linear_velocity.normalized() * initial_speed
